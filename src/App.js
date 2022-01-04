0e8b12cc83445345e93f8e280f82fdcc7c28ac4c
jcamilov <vargasjcamilo@gmail.com>
2022-01-04 14:50:47 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <div class="container">
+        <h1>My app</h1>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
